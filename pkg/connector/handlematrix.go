bc18a16d677ed8efd4bd7ae58f83c193c5d65ca6
YourSandwich <YourSandwich@users.noreply.github.com>
2026-04-19 23:22:58 +0200

    feat(connector): forward matrix text messages to teams

---
+++ pkg/connector/handlematrix.go
@@ -0,0 +1,66 @@
+// mautrix-teams - A Matrix-Microsoft Teams puppeting bridge.
+// Copyright (C) 2026 Sandwich
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+package connector
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"maunium.net/go/mautrix/bridgev2"
+	"maunium.net/go/mautrix/bridgev2/database"
+	"maunium.net/go/mautrix/event"
+
+	"go.mau.fi/mautrix-teams/pkg/msteams"
+	"go.mau.fi/mautrix-teams/pkg/teamsid"
+)
+
+func (t *TeamsClient) HandleMatrixMessage(ctx context.Context, msg *bridgev2.MatrixMessage) (*bridgev2.MatrixMessageResponse, error) {
+	if !t.IsLoggedIn() {
+		return nil, bridgev2.ErrNotLoggedIn
+	}
+	threadID := teamsid.ParsePortalID(msg.Portal.ID)
+	if threadID == "" {
+		return nil, errors.New("invalid portal id")
+	}
+	opts := msteams.SendOptions{
+		DisplayName: t.UserLogin.Metadata.(*UserLoginMetadata).DisplayName,
+	}
+	body, ct := t.matrixContentToTeams(msg.Content)
+	opts.ContentType = ct
+	id, err := t.Client.SendMessage(ctx, threadID, body, opts)
+	if err != nil {
+		return nil, err
+	}
+	return &bridgev2.MatrixMessageResponse{
+		DB: &database.Message{
+			ID:        teamsid.MakeMessageID(threadID, id),
+			SenderID:  teamsid.MakeUserID(t.UserMRI),
+			Timestamp: time.Now(),
+		},
+	}, nil
+}
+
+// matrixContentToTeams returns (body, contentType) for an outbound Matrix message.
+func (t *TeamsClient) matrixContentToTeams(content *event.MessageEventContent) (body, contentType string) {
+	if content == nil {
+		return "", "text"
+	}
+	if content.Format == event.FormatHTML && content.FormattedBody != "" {
+		return msteams.MatrixToTeamsHTML(content.FormattedBody), "html"
+	}
+	return content.Body, "text"
+}
